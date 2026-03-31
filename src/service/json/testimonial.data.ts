3486ec6fc71f41fb56fb88e132741f206027f285
akshukisku <akshay.kisku4@gmail.com>
2026-03-31 15:43:31 +0530

    fix the all images json

--- src/service/json/testimonial.data.ts
+++ src/service/json/testimonial.data.ts
@@ -1,9 +1,14 @@
+import char1 from "../../assets/char/char_1.jpeg";
+import char2 from "../../assets/char/char_2.jpeg";
+import char3 from "../../assets/char/char_3.jpeg";
+import char4 from "../../assets/char/char_4.jpeg";
+
 export const testimonials = [
   {
     id: 1,
     name: "James Anderson",
     role: "Customer",
-    image: "src/assets/char/char_1.jpeg",
+    image: char1,
     message:
       "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.",
   },
@@ -11,7 +16,7 @@ export const testimonials = [
     id: 2,
     name: "Sophia Carter",
     role: "Customer",
-    image: "src/assets/char/char_2.jpeg",
+    image: char2,
     message:
       "Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text.",
   },
@@ -19,7 +24,7 @@ export const testimonials = [
     id: 3,
     name: "Michael Thompson",
     role: "Customer",
-    image: "src/assets/char/char_3.jpeg",
+    image: char3,
     message:
       "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in classical Latin literature.",
   },
@@ -27,7 +32,7 @@ export const testimonials = [
     id: 4,
     name: "Emily Rodriguez",
     role: "Customer",
-    image: "src/assets/char/char_4.jpeg",
+    image: char4,
     message:
       "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
   },
